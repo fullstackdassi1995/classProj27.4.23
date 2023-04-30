6a54b76aefdc7b384b88233f5e9204e2a570c21b
fullstackdassi1995 <112422387+fullstackdassi1995@users.noreply.github.com>
2023-05-01 01:19:03 +0300

    Initial commit

---
+++ src/main/java/org/example/Hapoalim.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Pohalim extends Bankim{
+    public Pohalim(String id, String bankName, int numberOfEmployees, int sumHachnasa, int sumOtzaot, ArrayList<Customer> customer) {
+        super(id, bankName, numberOfEmployees, sumHachnasa, sumOtzaot, customer);
+    }
+}
